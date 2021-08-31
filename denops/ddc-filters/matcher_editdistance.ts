4a6efd8113a6f8329740b0b7a14b665a5f306e59
matsui54 <haru.matu9168@gmail.com>
2021-08-31 17:56:57 +0900

    Update for the latest ddc

--- denops/ddc-filters/matcher_editdistance.ts
+++ denops/ddc-filters/matcher_editdistance.ts
@@ -1,8 +1,5 @@
-import {
-  BaseFilter,
-  Candidate,
-} from "./deps.ts";
-import { editDistance } from "./editDistance.ts";
+import { BaseFilter, Candidate } from "../editdistance/deps.ts";
+import { editDistance } from "../editdistance/editDistance.ts";
 
 type Params = {
   limit: number;
