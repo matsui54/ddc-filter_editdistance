f353ec5df9306f7327a0081a8a44355b9b0dcf63
matsui54 <haru.matu9168@gmail.com>
2021-09-03 17:14:53 +0900

    Update deps

--- denops/editdistance/deps.ts
+++ denops/editdistance/deps.ts
@@ -1,2 +1,2 @@
-export * from "https://deno.land/x/ddc_vim@v0.4.2/types.ts#^";
-export * from "https://deno.land/x/ddc_vim@v0.4.2/deps.ts#^";
+export * from "https://deno.land/x/ddc_vim@v0.5.0/types.ts#^";
+export * from "https://deno.land/x/ddc_vim@v0.5.0/deps.ts#^";
