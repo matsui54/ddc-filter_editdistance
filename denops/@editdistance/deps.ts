d91af85e8b62eb5cba8dd77e55c1e13f38bcbe6a
matsui54 <matsui54@users.noreply.github.com>
2022-05-04 00:50:52 +0000

    :package: Update Deno dependencies
    Update dependencies by udd:
    
    /home/runner/work/ddc-filter_editdistance/ddc-filter_editdistance/denops/@editdistance/benchmark.ts
    [1/1] Looking for releases: https://deno.land/std@0.136.0/testing/bench.ts
    [1/1] Attempting update: https://deno.land/std@0.136.0/testing/bench.ts -> 0.137.0
    [1/1] Update successful: https://deno.land/std@0.136.0/testing/bench.ts -> 0.137.0
    
    /home/runner/work/ddc-filter_editdistance/ddc-filter_editdistance/denops/@editdistance/deps.ts
    [1/2] Looking for releases: https://deno.land/x/ddc_vim@v2.2.0/types.ts
    [1/2] Attempting update: https://deno.land/x/ddc_vim@v2.2.0/types.ts -> v2.3.0
    [1/2] Update successful: https://deno.land/x/ddc_vim@v2.2.0/types.ts -> v2.3.0
    [2/2] Looking for releases: https://deno.land/x/ddc_vim@v2.2.0/deps.ts
    [2/2] Attempting update: https://deno.land/x/ddc_vim@v2.2.0/deps.ts -> v2.3.0
    [2/2] Update successful: https://deno.land/x/ddc_vim@v2.2.0/deps.ts -> v2.3.0
    
    /home/runner/work/ddc-filter_editdistance/ddc-filter_editdistance/denops/@editdistance/editDistance.ts
    
    /home/runner/work/ddc-filter_editdistance/ddc-filter_editdistance/denops/@editdistance/test.ts
    
    /home/runner/work/ddc-filter_editdistance/ddc-filter_editdistance/denops/@ddc-filters/sorter_editdistance.ts
    
    /home/runner/work/ddc-filter_editdistance/ddc-filter_editdistance/denops/@ddc-filters/matcher_editdistance.ts
    
    /home/runner/work/ddc-filter_editdistance/ddc-filter_editdistance/README.md
    
    Successfully updated:
    https://deno.land/std@0.136.0/testing/bench.ts 0.136.0 -> 0.137.0
    https://deno.land/x/ddc_vim@v2.2.0/types.ts v2.2.0 -> v2.3.0
    https://deno.land/x/ddc_vim@v2.2.0/deps.ts v2.2.0 -> v2.3.0
    make[1]: Entering directory '/home/runner/work/ddc-filter_editdistance/ddc-filter_editdistance'
    make[1]: Leaving directory '/home/runner/work/ddc-filter_editdistance/ddc-filter_editdistance'

--- denops/@editdistance/deps.ts
+++ denops/@editdistance/deps.ts
@@ -1,2 +1,2 @@
-export * from "https://deno.land/x/ddc_vim@v2.2.0/types.ts";
-export * from "https://deno.land/x/ddc_vim@v2.2.0/deps.ts";
+export * from "https://deno.land/x/ddc_vim@v2.3.0/types.ts";
+export * from "https://deno.land/x/ddc_vim@v2.3.0/deps.ts";
