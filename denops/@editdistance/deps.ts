b9e961638a85e988ed9530b5a035f117c8696507
matsui54 <haru.matu9168@gmail.com>
2023-01-01 14:42:24 +0900

    📦 Update Deno dependencies

--- denops/@editdistance/deps.ts
+++ denops/@editdistance/deps.ts
@@ -1,2 +1,2 @@
-export * from "https://deno.land/x/ddc_vim@v3.1.0/types.ts";
-export * from "https://deno.land/x/ddc_vim@v3.1.0/deps.ts";
+export * from "https://deno.land/x/ddc_vim@v3.4.0/types.ts";
+export * from "https://deno.land/x/ddc_vim@v3.4.0/deps.ts";
